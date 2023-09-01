7b8c2479a87ebb9b06ddc39e4cecc7ec119afb9a
Natanael Barbosa Santos <104917678+natanael-b@users.noreply.github.com>
2023-09-01 14:05:13 -0300

    Improve sandbox reliability

--- init.lua
+++ init.lua
@@ -8,7 +8,47 @@ local Extension_Pages = {}
 
 local Registered_Extensions = {}
 
+local function clone_table(orig)
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        copy = {}
+        for orig_key, orig_value in next, orig, nil do
+            copy[clone_table(orig_key)] = clone_table(orig_value)
+        end
+        setmetatable(copy, clone_table(getmetatable(orig)))
+    else
+        copy = orig
+    end
+    return copy
+end
+
+-- Remove recursive references
+_G = nil
+package.loaded._G = nil
+package.loaded.package = nil
+
+-- Remove the _ENV metatable te ensure that onle LuaWPP metatable will be loaded
+
+setmetatable(_ENV,nil)
+
+-- Clone the global variable state 
+
+local __ENV  = clone_table(_ENV)
+local __loadedpackage  = clone_table(package.loaded)
+
+local function dirname(name)
+    local directory = {Pages.output}
+    for folder in __ENV.string.gmatch(name,"[^/]+") do
+        directory[#directory+1] = folder
+    end
+    __ENV.table.remove(directory,#directory)
+
+    return __ENV.table.concat(directory,"/")
+end
+
 local function mkdir(path)
+    local path = dirname(path)
     -- Windows
     if package.config:sub(1,1) == "\\" then
         os.execute('md "'..path..'" >nul 2>nul')
@@ -18,67 +58,26 @@ local function mkdir(path)
     os.execute("mkdir -p '"..path.."' >/dev/null 2>&1")
 end
 
-local original_ENV  = {}
-local original_libs = {}
-local original_meta = {}
 
-for name,value in pairs(_ENV) do
-    if type(value) == "table" then
-        original_libs[name] = {}
-        local lib = original_libs[name]
-        for name, _value in pairs(value) do
-            lib[name] = _value
-        end
-
-        original_meta[name] = {}
-        lib = original_meta[name]
-        for name, _value in pairs(getmetatable(value) or {}) do
-            lib[name] = _value
-        end
-    else
-        original_ENV[name] = value
-    end
-end
-
-local function dirname(name)
-    local directory = {Pages.output}
-    for folder in original_libs.string.gmatch(name,"[^/]+") do
-        directory[#directory+1] = folder
-    end
-    original_libs.table.remove(directory,#directory)
-
-    return original_libs.table.concat(directory,"/")
-end
-
-for i, page in original_ENV.ipairs(Pages) do
+for i, page in __ENV.ipairs(Pages) do
     print("Generating "..i.."/"..#Pages..": "..page..".html")
+    mkdir(page)
 
-    original_ENV.setmetatable(_ENV,{})
+    __ENV.setmetatable(_ENV,nil)
 
-    for variable in pairs(_ENV) do
-        _ENV[variable] = nil
+    for key, value in ipairs(clone_table(__ENV)) do
+        _ENV[key] = value
     end
 
-    for name, fn in original_ENV.pairs(original_ENV) do
-        _ENV[name] = fn
-    end
-
-    for name, lib in original_ENV.pairs(original_libs) do
-        _ENV[name] = {}
-        local newlib = _ENV[name]
-        for name_, fn in pairs(lib) do
-            newlib[name_] = fn
-            original_ENV.setmetatable(newlib,nil)
+    -- Unload LuaWPP libraries
+    for pkg in pairs(package.loaded) do
+        if __loadedpackage[pkg] == nil then
+            package.loaded[pkg] = nil
         end
     end
 
-    local path = dirname(page)
+    require "lua-wpp-framework.LuaTML"
 
-    if path ~= "" then
-        mkdir(path)
-    end
-
-    original_ENV.require "lua-wpp-framework.LuaTML"
     for i, name in ipairs(Extensions or {}) do
         require("lua-wpp-framework.extensions."..name)
         if Registered_Extensions["lua-wpp-framework.extensions."..name] == nil then
@@ -91,9 +90,10 @@ for i, page in original_ENV.ipairs(Pages) do
         
         RegisterPlugin = nil
     end
-    original_ENV.require (Pages.sources.."."..page:gsub("/","."))
+    
+    require(Pages.sources.."."..page:gsub("/","."))
 
-    local html_file = original_libs["io"].open(Pages.output.."/"..page..".html","w")
+    local html_file = __ENV.io.open(Pages.output.."/"..page..".html","w")
 
     if html_file then
         html_file:write(type(__HTML__) == "string" and __HTML__ or "")
@@ -102,12 +102,9 @@ for i, page in original_ENV.ipairs(Pages) do
 end
 
 for filename, content in pairs(Extension_Pages) do
-    local path = dirname(filename)
-    if path ~= "" then
-        mkdir(path)
-    end
+    mkdir(filename)
 
-    local html_file = original_libs["io"].open(Pages.output.."/"..filename,"w")
+    local html_file = __ENV.io.open(Pages.output.."/"..filename,"w")
 
     if html_file then
         html_file:write(tostring(content or ""))
